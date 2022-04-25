a7b50b1787a25d24d5fa266ced945eebaccbd55d
Kuchumova Vera <weryn4ik86@gmail.com>
2022-04-25 12:09:16 +0300

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price =5624; // стоимость билета
+        int bonusPrice =20; //количество рублей для одной бонусной милли
+        int mile = price/bonusPrice; // количество бонусных миль
+
+        System.out.println(mile);
+    }
+}
