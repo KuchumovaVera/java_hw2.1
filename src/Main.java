9005d7556f61c6ddb4fec9ed21acf4c9e51c12f5
Kuchumova Vera <weryn4ik86@gmail.com>
2022-04-25 12:37:37 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double price =5624.25; // стоимость билета
-        int bonusPrice =20; //количество рублей для одной бонусной милли
-        int mile = (int)price/bonusPrice; // количество бонусных миль
+        double price = 5624.25; // стоимость билета
+        int bonusPrice = 20; //количество рублей для одной бонусной милли
+        int mile = (int) price / bonusPrice; // количество бонусных миль
 
         System.out.println(mile);
     }
