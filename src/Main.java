cf12abaa39eb36fc4095f4aec8b412725be564a0
Kuchumova Vera <weryn4ik86@gmail.com>
2022-04-25 12:16:27 +0300

    second version

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price =5624; // стоимость билета
+        double price =5624.25; // стоимость билета
         int bonusPrice =20; //количество рублей для одной бонусной милли
-        int mile = price/bonusPrice; // количество бонусных миль
+        int mile = (int)price/bonusPrice; // количество бонусных миль
 
         System.out.println(mile);
     }
